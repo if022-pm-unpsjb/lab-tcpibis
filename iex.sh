ea97124acec7fe7ab32388faa8b7c82003a7aa25
Maia <maichcornejo@gmail.com>
2025-08-28 17:28:05 -0300

    antes del pull

--- iex.sh
+++ iex.sh
@@ -2,4 +2,4 @@
 # you can mount a local directory into the container.
 # Make sure to specify the user with "-u $(id -u):$(id -g)"
 # so that the created files have proper permissions
-docker run -it --rm -v "$(pwd)":/app -w /app -u $(id -u):$(id -g) --network host elixir:alpine iex -S mix
+docker run -it --rm -v "$(pwd)":/app -w /app -u $(id -u):$(id -g) elixir:alpine iex -S mix
