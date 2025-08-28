9622dd14924c39fa6b86ff0f9e10038e5a0f6f6f
ericnob1974 <ericnob1974@gmail.com>
2025-08-28 17:27:22 -0300

    Servidor de infracciones

---
+++ lib/infracciones.ex
@@ -0,0 +1,58 @@
+defmodule Libremarket.Infracciones do
+
+  def detectar_infraccion(id_compra) do
+    Enum.random([:true, :false])
+  end
+
+end
+
+defmodule Libremarket.Infracciones.Server do
+  @moduledoc """
+  Infracciones
+  """
+
+  use GenServer
+
+  # API del cliente
+
+  @doc """
+  Crea un nuevo servidor de Infracciones
+  """
+  def start_link(opts \\ %{}) do
+    GenServer.start_link(__MODULE__, opts, name: __MODULE__)
+  end
+
+  def detectar_infraccion(pid \\ __MODULE__, id_compra) do
+    GenServer.call(pid, {:detectar_infraccion, id_compra})
+  end
+  
+  def listar_infracciones() do
+    GenServer.call(__MODULE__, :listar_infracciones)
+  end
+
+  # Callbacks
+
+  @doc """
+  Inicializa el estado del servidor
+  """
+  @impl true
+  def init(state) do
+    {:ok, %{}}
+  end
+
+  @doc """
+  Callback para un call :detectar_infraccion
+  """
+  @impl true
+  def handle_call({:detectar_infraccion, id_compra}, _from, state) do
+    infraccion = Libremarket.Infracciones.detectar_infraccion(id_compra)
+    new_state = Map.put(state, id_compra, infraccion)
+    {:reply, infraccion, new_state}
+  end
+
+  @impl true
+  def handle_call(:listar_infracciones, _from, state) do
+    {:reply, state, state}
+  end
+
+end
