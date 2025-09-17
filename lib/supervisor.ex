ea21e52ae9754899cf13b53c881c840020b9840a
Facundo <facuespaniol@gmail.com>
2025-09-17 19:57:56 -0300

    Fin erpc

--- lib/supervisor.ex
+++ lib/supervisor.ex
@@ -11,9 +11,10 @@ defmodule Libremarket.Supervisor do
   @impl true
   def init(_opts) do
 
-    server_to_run = [
-      String.to_existing_atom(System.get_env("SERVER_TO_RUN"))
-    ]
+    server_to_run = case System.get_env("SERVER_TO_RUN") do
+      nil -> []
+      server_to_run -> [{String.to_existing_atom(server_to_run), %{}}]
+    end
 
     Supervisor.init(server_to_run, strategy: :one_for_one)
   end
