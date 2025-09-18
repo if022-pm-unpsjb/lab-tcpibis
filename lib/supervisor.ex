491d2bb6f2e928b8013cc7363a1f5378c1f06014
Facundo <facuespaniol@gmail.com>
2025-09-17 23:13:10 -0300

    Cluster no levanta

--- lib/supervisor.ex
+++ lib/supervisor.ex
@@ -11,11 +11,28 @@ defmodule Libremarket.Supervisor do
   @impl true
   def init(_opts) do
 
+    topologies = [
+      gossip: [
+        strategy: Cluster.Strategy.Gossip,
+        config: [
+          port: 45892,
+          if_addr: "0.0.0.0",
+          multicast_addr: "127.0.0.1",
+          broadcast_only: true,
+          secret: "secret"
+        ]
+      ]
+    ]
+
     server_to_run = case System.get_env("SERVER_TO_RUN") do
       nil -> []
       server_to_run -> [{String.to_existing_atom(server_to_run), %{}}]
     end
 
-    Supervisor.init(server_to_run, strategy: :one_for_one)
+    childrens = [
+      {Cluster.Supervisor, [topologies, [name: Libremarket.ClusterSupervisor]]},
+    ] ++ server_to_run
+
+    Supervisor.init(childrens, strategy: :one_for_one)
   end
 end
