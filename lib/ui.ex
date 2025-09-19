0b1522b1d89e5579bcd4672c7535316d81954b6d
ericnob1974 <ericnob1974@gmail.com>
2025-09-19 17:14:23 -0300

    Libcluster añadido correctamente

--- lib/ui.ex
+++ lib/ui.ex
@@ -1,7 +1,7 @@
 defmodule Libremarket.Ui do
 
   def comprar(id_producto, forma_de_entrega, medio_de_pago) do
-    :erpc.call(:"compras@compras", Libremarket.Compras.Server, :comprar, [id_producto, forma_de_entrega, medio_de_pago])
+    Libremarket.Compras.Server.comprar(id_producto, forma_de_entrega, medio_de_pago)
   end
 #modulo y funciones wue no ejecutan como un servidor solo llaman a otras funciones
 #selecciona el producto, la forma de entrega y el medio de pago llamando a los servidores
